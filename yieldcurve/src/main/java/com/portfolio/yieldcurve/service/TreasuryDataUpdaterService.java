7805e434ec074fa7fc9621fcb12a94ba1cf4ad22
Agustin Biondi <agustin.biondi@gmail.com>
2024-11-25 11:39:31 -0500

    t

--- yieldcurve/src/main/java/com/portfolio/yieldcurve/service/TreasuryDataUpdaterService.java
+++ yieldcurve/src/main/java/com/portfolio/yieldcurve/service/TreasuryDataUpdaterService.java
@@ -13,7 +13,6 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 
 @Service
 public class TreasuryDataUpdaterService {
