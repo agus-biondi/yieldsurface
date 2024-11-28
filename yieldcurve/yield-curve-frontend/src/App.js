1392d410b0b1f61136cc4a4f2b28e8b85efce902
Agustin Biondi <agustin.biondi@gmail.com>
2024-11-28 00:49:28 -0500

    removed animation test

--- yieldcurve/yield-curve-frontend/src/App.js
+++ yieldcurve/yield-curve-frontend/src/App.js
@@ -1,12 +1,10 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import OriginalApp from "./components/OriginalApp";
-import AnimatedApp from "./components/AnimatedApp";
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/animated" element={<AnimatedApp />} />
         <Route path="/" element={<OriginalApp/>} />
       </Routes>
     </Router>
