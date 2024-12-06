ffc38699f643c1ef3d765ae97544ed19cd2788b7
Agustin Biondi <agustin.biondi@gmail.com>
2024-12-06 14:07:36 -0500

    redeploy

--- yieldcurve/src/main/java/com/portfolio/yieldcurve/config/WebConfig.java
+++ yieldcurve/src/main/java/com/portfolio/yieldcurve/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.portfolio.yieldcurve.config;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -8,12 +9,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${frontend.url}")
+    private String frontendUrl;
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(
-                        "http://localhost:3000",
-                        "https://yield-curve-surface-e24343dd0c34.herokuapp.com/")
+                .allowedOrigins(frontendUrl)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
